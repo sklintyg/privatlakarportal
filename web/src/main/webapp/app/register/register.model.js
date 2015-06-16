05e6240d9d2daf53fe4017118cb82bf3d90c80ba
Benny Samuelsson <benny.samuelsson@usify.se>
2015-06-16 14:09:22 +0200

    Lade in steg 2 och 3.

--- web/src/main/webapp/app/register/register.model.js
+++ web/src/main/webapp/app/register/register.model.js
@@ -4,11 +4,22 @@ angular.module('privatlakareApp').factory('RegisterModel',
 
         return {
             reset: function() {
+                // Step 1
                 this.befattning = RegisterViewStateService.befattningList[0];
                 this.verksamhetensNamn = null;
+                this.agandeForm = 'Privat';
                 this.vardform = RegisterViewStateService.vardformList[0];
-                this.verksamhetstyp = RegisterViewStateService.verksamhetstypList[0];;
+                this.verksamhetstyp = RegisterViewStateService.verksamhetstypList[0];
                 this.arbetsplatskod = null;
+
+                // Step 2
+                this.telefonnummer = null;
+                this.epost = null;
+                this.epost2 = null;
+                this.adress = null;
+                this.postnummer = null;
+                this.postort = null;
+                this.kommun = null;
                 return this;
             }
         };
--- web/src/main/webapp/app/register/step3/step3.controller.js
+++ web/src/main/webapp/app/register/step3/step3.controller.js
@@ -1,6 +1,32 @@
 'use strict';
 
 angular.module('privatlakareApp')
-  .controller('Step3Ctrl', function ($scope, RegisterViewStateService) {
+  .controller('Step3Ctrl', function ($scope, RegisterViewStateService, UserModel, RegisterModel) {
         RegisterViewStateService.updateStep();
+        
+        $scope.uppgifter = [
+            { name: 'Personnummer', value: UserModel.personnummer },
+            { name: 'Namn', value: UserModel.name },
+            { name: 'Befattning', value: RegisterModel.befattning },
+            { name: 'Verksamhetens namn', value: RegisterModel.verksamhetensNamn },
+            { name: 'Ägandeform', value: RegisterModel.agandeForm },
+            { name: 'Vårdform', value: RegisterModel.vardform },
+            { name: 'Verksamhetstyp', value: RegisterModel.verksamhetstyp },
+            { name: 'Arbetsplatskod', value: RegisterModel.arbetsplatskod }
+        ];
+
+        $scope.kontaktUppgifter = [
+            { name: 'Telefonnummer', value: RegisterModel.telefonnummer },
+            { name: 'E-post', value: RegisterModel.epost },
+            { name: 'Adress', value: RegisterModel.adress },
+            { name: 'Postnummer', value: RegisterModel.postnummer },
+            { name: 'Postort', value: RegisterModel.postort },
+            { name: 'Kommun', value: RegisterModel.kommun }
+        ];
+
+        $scope.socialstyrelsenUppgifter = [
+            { name: 'Legimiterad yrkesgrupp', value: RegisterModel.legitimeradYrkesgrupp },
+            { name: 'Specialitet', value: RegisterModel.specialitet },
+            { name: 'Förskrivarkod', value: RegisterModel.forskrivarkod }
+        ];
   });
