b61925a214c8dc7857a917f01d4682fd759098cb
Erik Forsgren <erik.forsgren@r2m.se>
2015-06-17 12:34:41 +0200

    Initial checkin för wc-integration

---
+++ integration/wc-integration/src/main/java/se/inera/privatlakarportal/integration/services/GetPrivatePractitioner.java
@@ -0,0 +1,8 @@
+package se.inera.privatlakarportal.integration.services;
+
+import se.riv.infrastructure.directory.privatepractitioner.getprivatepractitionerresponder.v1.GetPrivatePractitionerResponseType;
+
+public interface GetPrivatePractitioner {
+
+    GetPrivatePractitionerResponseType getPrivatePractitioner();
+}
