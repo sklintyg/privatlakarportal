bc31546fd12c38f282e73a5f18fd3ce2354a6445
Erik Forsgren <erik.forsgren@r2m.se>
2015-06-22 12:50:45 +0200

    Fix

--- web/src/main/java/se/inera/privatlakarportal/config/WebConfig.java
+++ web/src/main/java/se/inera/privatlakarportal/config/WebConfig.java
@@ -11,6 +11,7 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -23,11 +24,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
-
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setViewClass(JstlView.class);
         viewResolver.setPrefix("/");
-        viewResolver.setSuffix(".html");
+        viewResolver.setSuffix("");
 
         registry.viewResolver(viewResolver);
     }
@@ -43,14 +43,19 @@ public class WebConfig extends WebMvcConfigurerAdapter {
       // Add other converters ...
     }
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        int cachePeriod = 3600 * 24 * 15;
+        registry.addResourceHandler("/*.html").addResourceLocations("/").setCachePeriod(cachePeriod);
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("/").setCachePeriod(cachePeriod);
+        registry.addResourceHandler("/robots.txt").addResourceLocations("/").setCachePeriod(cachePeriod);
+        registry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/");
 //        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
 //        registry.addResourceHandler("/vendor/**").addResourceLocations("/vendor/");
 //        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
 //        registry.addResourceHandler("/partials/**").addResourceLocations(
 //                "/partials/");
-//    }
+    }
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
--- web/src/main/java/se/inera/privatlakarportal/web/controller/WebController.java
+++ web/src/main/java/se/inera/privatlakarportal/web/controller/WebController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-    @RequestMapping(value = "/", method = GET)
+    @RequestMapping(value = "html", method = GET, produces = "text/plain")
     public String getIndexPage() {
-        return "index";
+        return "/index.html";
     }
 
 }
