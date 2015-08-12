316ebb693c8b6996e94f7a35fe3e2c88109b52cf
Per Beijer <per.beijer@usify.se>
2015-08-12 12:46:56 +0200

    PRIV-20 Läs in postort,kommun och län från postnummerfil. Dessa kan slås upp från PostnummerService med inkommande postnummer.

---
+++ web/src/test/java/se/inera/privatlakarportal/service/postnummer/repo/PostnummerRepositoryFactoryTest.java
@@ -0,0 +1,40 @@
+package se.inera.privatlakarportal.service.postnummer.repo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import se.inera.privatlakarportal.service.postnummer.model.Omrade;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by pebe on 2015-08-12.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class PostnummerRepositoryFactoryTest {
+
+    private PostnummerRepositoryFactory factory = new PostnummerRepositoryFactory();
+
+    private static final String LINE_1 = "13100,NACKA,01,STOCKHOLM,0182,NACKA,01";
+    private static final String LINE_1_POSTNUMMER = "13100";
+    private static final String LINE_1_POSTORT = "NACKA";
+    private static final String LINE_1_LAN = "STOCKHOLM";
+    private static final String LINE_1_KOMMUN = "NACKA";
+
+    @Test
+    public void testCreateOmradeFromString() {
+
+        Omrade res = factory.createOmradeFromString(LINE_1);
+
+        assertNotNull(res);
+        assertEquals(LINE_1_POSTNUMMER, res.getPostnummer());
+        assertEquals(LINE_1_POSTORT, res.getPostort());
+        assertEquals(LINE_1_KOMMUN, res.getKommun());
+        assertEquals(LINE_1_LAN, res.getLan());
+
+    }
+}
