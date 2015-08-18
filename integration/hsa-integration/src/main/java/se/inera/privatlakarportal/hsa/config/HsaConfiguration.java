c71c0b5a946a295ef38acde970a847c4140e7377
Per Beijer <per.beijer@usify.se>
2015-08-18 15:33:12 +0200

    HttpConduit konfiguration för HSA integration

--- integration/hsa-integration/src/main/java/se/inera/privatlakarportal/hsa/config/HsaConfiguration.java
+++ integration/hsa-integration/src/main/java/se/inera/privatlakarportal/hsa/config/HsaConfiguration.java
@@ -1,31 +1,35 @@
 package se.inera.privatlakarportal.hsa.config;
 
-import org.apache.cxf.Bus;
-import org.apache.cxf.endpoint.Client;
-import org.apache.cxf.jaxws.JaxWsClientFactoryBean;
+import org.apache.cxf.configuration.jsse.TLSClientParameters;
+import org.apache.cxf.configuration.security.FiltersType;
+import org.apache.cxf.jaxws.JaxWsClientProxy;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.ConnectionType;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 import se.inera.ifv.hsaws.v3.HsaWsResponderInterface;
 import se.inera.ifv.privatlakarportal.spi.authorization.impl.HSAWebServiceCalls;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 
 @Configuration
 @ComponentScan("se.inera.privatlakarportal.hsa.services")
 @Import(HsaStubConfiguration.class)
 public class HsaConfiguration {
-
-    private static final Logger LOG = LoggerFactory.getLogger(HsaConfiguration.class);
-
-
-    @Autowired
-    private ApplicationContext applicationContext;
 
     @Value("${hsa.ws.service.logicaladdress}")
     private String hsaLogicalAddress;
@@ -33,12 +37,28 @@ public class HsaConfiguration {
     @Value("${hsa.ws.services.url}")
     private String hsaWsUrl;
 
+    @Value("${hsa.ws.key.manager.password}")
+    String hsaWsKeyManagerPassword;
+
+    @Value("${hsa.ws.certificate.file)")
+    String hsaWsCertificateFile;
+    @Value("${hsa.ws.certificate.password)")
+    String hsaWsCertificatePassword;
+    @Value("${hsa.ws.certificate.type)")
+    String hsaWsCertificateType;
+
+    @Value("${hsa.ws.truststore.file)")
+    String hsaWsTruststoreFile;
+    @Value("${hsa.ws.truststore.password)")
+    String hsaWsTruststorePassword;
+    @Value("${hsa.ws.truststore.type)")
+    String hsaWsTruststoreType;
+
     @Bean
     public HsaWsResponderInterface hsaWebServiceClient() {
         JaxWsProxyFactoryBean proxyFactoryBean = new JaxWsProxyFactoryBean();
         proxyFactoryBean.setAddress(hsaWsUrl);
         proxyFactoryBean.setServiceClass(HsaWsResponderInterface.class);
-
         return (HsaWsResponderInterface) proxyFactoryBean.create();
     }
 
@@ -47,6 +67,48 @@ public class HsaConfiguration {
         HSAWebServiceCalls hsaWebServiceCalls = new HSAWebServiceCalls();
         hsaWebServiceCalls.setHsaLogicalAddress(hsaLogicalAddress);
         return hsaWebServiceCalls;
+    }
+
+    @Bean
+    @Profile("!dev")
+    public HTTPConduit httpConduit() throws IOException, KeyStoreException, NoSuchAlgorithmException, UnrecoverableKeyException, CertificateException {
+        HTTPConduit httpConduit = (HTTPConduit) JaxWsClientProxy.getClient(hsaWebServiceClient()).getConduit();
+
+        HTTPClientPolicy client = new HTTPClientPolicy();
+        client.setAllowChunking(false);
+        client.setAutoRedirect(true);
+        client.setConnection(ConnectionType.KEEP_ALIVE);
+        httpConduit.setClient(client);
+
+        TLSClientParameters tlsClientParameters = new TLSClientParameters();
+        tlsClientParameters.setDisableCNCheck(true);
+
+        KeyStore trustStore = KeyStore.getInstance(hsaWsCertificateType);
+        trustStore.load(new FileInputStream(hsaWsCertificateFile), hsaWsCertificatePassword.toCharArray());
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(trustStore, hsaWsKeyManagerPassword.toCharArray());
+        tlsClientParameters.setKeyManagers(kmf.getKeyManagers());
+
+        KeyStore keyStore = KeyStore.getInstance(hsaWsTruststoreType);
+        keyStore.load(new FileInputStream(hsaWsTruststoreFile), hsaWsTruststorePassword.toCharArray());
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(keyStore);
+        tlsClientParameters.setTrustManagers(tmf.getTrustManagers());
+
+        FiltersType filters = new FiltersType();
+        filters.getInclude().add(".*_EXPORT_.*");
+        filters.getInclude().add(".*_EXPORT1024_.*");
+        filters.getInclude().add(".*_WITH_AES_256_.*");
+        filters.getInclude().add(".*_WITH_AES_128_.*");
+        filters.getInclude().add(".*_WITH_3DES_.*");
+        filters.getInclude().add(".*_WITH_DES_.*");
+        filters.getInclude().add(".*_WITH_NULL_.*");
+        filters.getInclude().add(".*_DH_anon_.*");
+        tlsClientParameters.setCipherSuitesFilter(filters);
+
+        httpConduit.setTlsClientParameters(tlsClientParameters);
+
+        return httpConduit;
     }
 
 }
