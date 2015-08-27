f2a1d8b53dbb7264a8395054c9e70f59712f3dbd
Benny Samuelsson <benny.samuelsson@usify.se>
2015-08-27 14:10:19 +0200

    PRIV-109 Som användare vill jag kunna se webcerts avtal innan jag registrerar mig. Webcert villkor kan nu hämtas och visas från servern.

--- web/src/main/webapp/app/terms/terms.controller.js
+++ web/src/main/webapp/app/terms/terms.controller.js
@@ -1,11 +1,14 @@
 angular.module('privatlakareApp')
-    .controller('MainTermsCtrl', function($scope, $http, $templateCache, $window) {
+    .controller('MainTermsCtrl', function($scope, $http, $templateCache, $window,
+        TermsProxy) {
         'use strict';
 
-        $http.get('app/terms/terms_text.html', { cache: $templateCache }).
-            success(function(data) {
-                $scope.terms = data;
-            });
+        TermsProxy.getTerms().then(function(successData) {
+            $scope.terms = successData.text;
+            $scope.version = successData.version;
+        }, function(errorData) {
+
+        });
 
         $scope.dismiss = function() {
             $scope.$dismiss();
