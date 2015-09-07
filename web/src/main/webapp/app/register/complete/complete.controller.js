19e9ba691cca84c0ed8a51513ac664851021ec3e
Benny Samuelsson <benny.samuelsson@usify.se>
2015-09-07 09:07:47 +0200

    PRIV-18 När en användare som registrerar sig redan finns i HOSP/HSA skall denne kunna logga in direkt

--- web/src/main/webapp/app/register/complete/complete.controller.js
+++ web/src/main/webapp/app/register/complete/complete.controller.js
@@ -1,5 +1,9 @@
 angular.module('privatlakareApp')
-  .controller('CompleteCtrl', function ($scope) {
-    'use strict';
-    $scope.message = 'Hello';
-  });
+    .controller('CompleteCtrl', function($scope, $window,
+        APP_CONFIG) {
+        'use strict';
+
+        $scope.goToApp = function() {
+            $window.location = APP_CONFIG.webcertStartUrl;
+        };
+    });
