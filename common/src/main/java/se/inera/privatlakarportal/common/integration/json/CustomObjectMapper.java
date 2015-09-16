8a9489b1b07f142336f918e10174b709ecde43f6
Per Beijer <per.beijer@usify.se>
2015-09-16 16:20:06 +0200

    properties med null värden ska komma med i jsonobjekten.

--- common/src/main/java/se/inera/privatlakarportal/common/integration/json/CustomObjectMapper.java
+++ common/src/main/java/se/inera/privatlakarportal/common/integration/json/CustomObjectMapper.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.datatype.joda.ser.LocalDateTimeSerializer;
 public class CustomObjectMapper extends ObjectMapper {
 
     public CustomObjectMapper() {
-        setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        setSerializationInclusion(JsonInclude.Include.ALWAYS);
         configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         registerModule(new Module());
