d5ddebc8e45a64d0bbf0139b1410ba9847ddfc54
Erik Forsgren <erik.forsgren@r2m.se>
2015-09-16 15:58:16 +0200

    PRIV-150 Ändra hur MimeMessage konstrueras

--- common/src/main/java/se/inera/privatlakarportal/common/service/MailServiceImpl.java
+++ common/src/main/java/se/inera/privatlakarportal/common/service/MailServiceImpl.java
@@ -3,17 +3,17 @@ package se.inera.privatlakarportal.common.service;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.activation.DataHandler;
+//import javax.activation.DataHandler;
 import javax.activation.DataSource;
-import javax.mail.BodyPart;
+//import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.Part;
+//import javax.mail.Multipart;
+//import javax.mail.Part;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
+//import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+//import javax.mail.internet.MimeMultipart;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.cxf.attachment.ByteDataSource;
@@ -21,7 +21,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -58,7 +60,7 @@ public class MailServiceImpl implements MailService {
 
     private static final String BOTTOM_BODY_CONTENT = "<br/><br/><span><img src='cid:inera_logo'></span>";
 
-    private static final String ENCODING = "text/html; charset=UTF-8";
+    //private static final String ENCODING = "text/html; charset=UTF-8";
 
     private static final Logger LOG = LoggerFactory.getLogger(MailServiceImpl.class);
 
@@ -71,15 +73,16 @@ public class MailServiceImpl implements MailService {
         LOG.info("Sending registration status email to {}", privatlakare.getEpost());
         try {
             MimeMessage message = createMessage(status, privatlakare);
+            message.saveChanges();
             mailSender.send(message);
-        } catch (MessagingException | PrivatlakarportalServiceException e) {
+        } catch (MessagingException | PrivatlakarportalServiceException | MailException e) {
             throw new PrivatlakarportalServiceException(PrivatlakarportalErrorCodeEnum.UNKNOWN_INTERNAL_PROBLEM, e.getMessage());
         }
     }
 
-    private MimeMessage createMessage(RegistrationStatus status, Privatlakare privatlakare) throws MessagingException, PrivatlakarportalServiceException {
+    private MimeMessage createMessage(RegistrationStatus status, Privatlakare privatlakare) throws MessagingException,
+            PrivatlakarportalServiceException {
         MimeMessage message = mailSender.createMimeMessage();
-        message.setHeader("Content-Type", ENCODING);
         message.setFrom(new InternetAddress(from));
         message.addRecipient(Message.RecipientType.TO,
                 new InternetAddress(privatlakare.getEpost()));
@@ -89,9 +92,10 @@ public class MailServiceImpl implements MailService {
     }
 
     private void buildEmailContent(MimeMessage message, RegistrationStatus status) throws MessagingException, PrivatlakarportalServiceException {
-        Multipart content = new MimeMultipart("related");
-        BodyPart body = new MimeBodyPart();
-        String subjectString = null; 
+       
+        // Multipart content = new MimeMultipart();
+        // BodyPart body = new MimeBodyPart();
+        String subjectString = null;
         String htmlBodyString = null;
 
         // Set correct content and subject depending on status
@@ -117,11 +121,11 @@ public class MailServiceImpl implements MailService {
         }
 
         htmlBodyString += BOTTOM_BODY_CONTENT;
-        body.setContent(htmlBodyString, ENCODING);
-        content.addBodyPart(body);
+//        body.setContent(htmlBodyString, ENCODING);
+//        content.addBodyPart(body);
 
-        // Add attachment and markup for logo 
-        MimeBodyPart iconBodyPart = new MimeBodyPart();
+        // Add attachment and markup for logo
+        // MimeBodyPart iconBodyPart = new MimeBodyPart();
 
         DataSource iconDataSource = null;
 
@@ -131,13 +135,18 @@ public class MailServiceImpl implements MailService {
             throw new PrivatlakarportalServiceException(PrivatlakarportalErrorCodeEnum.UNKNOWN_INTERNAL_PROBLEM,
                     "Internal I/O-error while building email.");
         }
-        iconBodyPart.setDataHandler(new DataHandler(iconDataSource));
-        iconBodyPart.setDisposition(Part.INLINE);
-        iconBodyPart.setContentID("<inera_logo>");
-        iconBodyPart.addHeader("Content-Type", "image/png");
-        content.addBodyPart(iconBodyPart);
+        // iconBodyPart.setDataHandler(new DataHandler(iconDataSource));
+        // iconBodyPart.setDisposition(Part.INLINE);
+        // iconBodyPart.setContentID("<inera_logo>");
+        // iconBodyPart.addHeader("Content-Type", "image/png");
+        // content.addBodyPart(iconBodyPart);
 
-        message.setContent(content, "UTF-8");
+        //Use mimeHelper to set content
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.addInline("<inera_logo>", iconDataSource);
+        helper.setText(htmlBodyString, true);
+        // message.setContent(content, ENCODING);
+
         message.setSubject(subjectString, "UTF-8");
     }
 
