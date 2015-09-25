e2b62ca776fcef291230185bdcd3e516160e74d0
Erik Forsgren <erik.forsgren@r2m.se>
2015-09-25 13:52:24 +0200

    Använd base-url från property

--- specifications/src/main/groovy/se/inera/privatlakarportal/page/RegisterPage.groovy
+++ specifications/src/main/groovy/se/inera/privatlakarportal/page/RegisterPage.groovy
@@ -1,6 +1,6 @@
 package se.inera.privatlakarportal.page
 
 class RegisterPage extends AbstractPage {
-    static url = "http://localhost:8090"
+    static url = System.getProperty("privatlakarportal.baseUrl")
     static at = { title == "Privatläkarportalen" }
 }
