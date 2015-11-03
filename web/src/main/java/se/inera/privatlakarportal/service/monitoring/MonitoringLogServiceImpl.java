9d20e97c8d2118f182c796e6a8b7d013c3972752
Erik Forsgren <erik.forsgren@r2m.se>
2015-11-03 14:17:51 +0100

    DEVENV: Tagit bort beroende på webcert-common

--- web/src/main/java/se/inera/privatlakarportal/service/monitoring/MonitoringLogServiceImpl.java
+++ web/src/main/java/se/inera/privatlakarportal/service/monitoring/MonitoringLogServiceImpl.java
@@ -23,8 +23,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import se.inera.certificate.logging.HashUtility;
-import se.inera.certificate.logging.LogMarkers;
+import se.inera.privatlakarportal.service.monitoring.util.HashUtility;
+import se.inera.privatlakarportal.service.monitoring.util.LogMarkers;
 import se.inera.privatlakarportal.common.model.RegistrationStatus;
 
 @Service("webMonitoringLogService")
