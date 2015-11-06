c384a222551f515babb133cd60ff6eeccd11ef9e
Benny Samuelsson <benny.samuelsson@usify.se>
2015-11-06 09:02:10 +0100

    PRIV-203: Länk "Gå tillbaka till Webcerts startsida" vid Tekniskt fel leder ej dit.

--- web/src/main/webapp/app/error/error.controller.js
+++ web/src/main/webapp/app/error/error.controller.js
@@ -1,6 +1,10 @@
 angular.module('privatlakareApp')
-    .controller('ErrorCtrl', function ($scope, $sessionStorage, $stateParams) {
+    .controller('ErrorCtrl', function ($scope, $sessionStorage, $stateParams,
+        APP_CONFIG) {
         'use strict';
+
+        $scope.webcertUrl = APP_CONFIG.webcertUrl;
+
         if ($stateParams.errorMessage) {
             $sessionStorage.errorMessage = $scope.errorMessage = $stateParams.errorMessage;
         }
