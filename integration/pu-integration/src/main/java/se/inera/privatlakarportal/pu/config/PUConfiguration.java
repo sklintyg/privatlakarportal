ca0a0de98f736cd7fd7b7c68f9e54a282ab0bce9
Per Beijer <per.beijer@usify.se>
2015-11-30 08:26:13 +0100

    PRIV-213: HttpConduit konfigurationen verkar inte fungera via javakonfig, flyttar till xml.

--- integration/pu-integration/src/main/java/se/inera/privatlakarportal/pu/config/PUConfiguration.java
+++ integration/pu-integration/src/main/java/se/inera/privatlakarportal/pu/config/PUConfiguration.java
@@ -32,27 +32,11 @@ import java.security.cert.CertificateException;
 @ComponentScan("se.inera.privatlakarportal.pu.services")
 @EnableCaching
 @Import(PUStubConfiguration.class)
+@ImportResource("classpath:pu-services-config.xml")
 public class PUConfiguration {
 
     @Value("${putjanst.endpoint.url}")
     String puWsUrl;
-
-    @Value("${ntjp.ws.key.manager.password}")
-    String ntjpWsKeyManagerPassword;
-
-    @Value("${ntjp.ws.certificate.file}")
-    String ntjpWsCertificateFile;
-    @Value("${ntjp.ws.certificate.password}")
-    String ntjpWsCertificatePassword;
-    @Value("${ntjp.ws.certificate.type}")
-    String ntjpWsCertificateType;
-
-    @Value("${ntjp.ws.truststore.file}")
-    String ntjpWsTruststoreFile;
-    @Value("${ntjp.ws.truststore.password}")
-    String ntjpWsTruststorePassword;
-    @Value("${ntjp.ws.truststore.type}")
-    String ntjpWsTruststoreType;
 
     @Bean
     public LookupResidentForFullProfileResponderInterface puWebServiceClient() {
@@ -74,55 +58,6 @@ public class PUConfiguration {
         EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
         ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
         return ehCacheManagerFactoryBean;
-    }
-
-    @Bean
-    @Profile("!dev")
-    public HTTPConduit httpConduit() throws IOException, KeyStoreException, NoSuchAlgorithmException, UnrecoverableKeyException, CertificateException {
-        HTTPConduit httpConduit = (HTTPConduit) JaxWsClientProxy.getClient(puWebServiceClient()).getConduit();
-
-        HTTPClientPolicy client = new HTTPClientPolicy();
-        client.setAllowChunking(false);
-        client.setAutoRedirect(true);
-        client.setConnection(ConnectionType.KEEP_ALIVE);
-        httpConduit.setClient(client);
-
-        TLSClientParameters tlsClientParameters = new TLSClientParameters();
-        tlsClientParameters.setDisableCNCheck(true);
-        KeyStore trustStore = KeyStore.getInstance(ntjpWsCertificateType);
-
-        try (FileInputStream trustStoreStream = new FileInputStream(ntjpWsCertificateFile)) {
-            trustStore.load(trustStoreStream, ntjpWsCertificatePassword.toCharArray());
-        }
-
-        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-        kmf.init(trustStore, ntjpWsKeyManagerPassword.toCharArray());
-        tlsClientParameters.setKeyManagers(kmf.getKeyManagers());
-
-        KeyStore keyStore = KeyStore.getInstance(ntjpWsTruststoreType);
-
-        try (FileInputStream keyStoreStream = new FileInputStream(ntjpWsTruststoreFile)) {
-            keyStore.load(keyStoreStream, ntjpWsTruststorePassword.toCharArray());
-        }
-
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-        tmf.init(keyStore);
-        tlsClientParameters.setTrustManagers(tmf.getTrustManagers());
-
-        FiltersType filters = new FiltersType();
-        filters.getInclude().add(".*_EXPORT_.*");
-        filters.getInclude().add(".*_EXPORT1024_.*");
-        filters.getInclude().add(".*_WITH_AES_256_.*");
-        filters.getInclude().add(".*_WITH_AES_128_.*");
-        filters.getInclude().add(".*_WITH_3DES_.*");
-        filters.getInclude().add(".*_WITH_DES_.*");
-        filters.getInclude().add(".*_WITH_NULL_.*");
-        filters.getInclude().add(".*_DH_anon_.*");
-        tlsClientParameters.setCipherSuitesFilter(filters);
-
-        httpConduit.setTlsClientParameters(tlsClientParameters);
-
-        return httpConduit;
     }
 
 }
