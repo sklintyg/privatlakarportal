2f669fbd5411818f4fb4a94c67c41c79400c8944
Per Beijer <per.beijer@usify.se>
2015-11-30 10:50:58 +0100

    PRIV-212: Städat unused imports.

--- web/src/main/java/se/inera/privatlakarportal/service/util/MedgivandeBootstrapBean.java
+++ web/src/main/java/se/inera/privatlakarportal/service/util/MedgivandeBootstrapBean.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Service;
