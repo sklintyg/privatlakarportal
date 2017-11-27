d5fa3e3c990ca0b407ae83a9f8e6b5179882e976
Benny Samuelsson <benny@solidcore.se>
2017-11-27 10:58:57 +0100

    INTYG-4840: Avbryt-knapp på start-sidan.

--- web/src/main/webapp/app/start/start.controller.js
+++ web/src/main/webapp/app/start/start.controller.js
@@ -1,4 +1,8 @@
 angular.module('privatlakareApp')
-    .controller('MainCtrl', function(/*$scope*/) {
+    .controller('MainCtrl', function($scope, $window) {
         'use strict';
+
+        $scope.toWebcertLandingPage = function(){
+            $window.location.href = 'https://webcert.intygstjanster.se/';
+        };
     });
