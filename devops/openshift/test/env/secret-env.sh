cb8197f48b3be5e857b2a9ae915cc088f07030e0
Peter Larsson <peter.larsson@callistaenterprise.se>
2018-11-05 13:15:42 +0100

    INTYG-6524: Använd java property för logback

--- devops/openshift/test/env/secret-env.sh
+++ devops/openshift/test/env/secret-env.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-export DB_USERNAME=${DATABASE_USERNAME:-intyg}
-export DB_PASSWORD=${DATABASE_PASSWORD:-intyg}
-export REDIS_PASSWORD=${REDIS_PASSWORD:-redis}
-
 export CATALINA_OPTS_APPEND="\
 -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE \
 -Dconfig.folder=/opt/$APP_NAME/config \
 -Dconfig.file=/opt/$APP_NAME/config/privatlakarportal.properties \
 -Dmailresource.file=/opt/$APP_NAME/config/mailresource.properties \
--Dlogback.file=$LOGBACK_FILE \
 -Dcertificate.folder=/opt/$APP_NAME/certifikat \
 -Dcredentials.file=/opt/$APP_NAME/env/secret-env.properties \
 -Dresources.folder=/tmp/resources \
