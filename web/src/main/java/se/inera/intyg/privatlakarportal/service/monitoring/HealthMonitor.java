4fceb5231c7ea245e1be077864a7a93512a77d6c
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-11-22 22:07:29 +0100

    INTYG-7636: Skrev om HealthMonitor så den uppdaterar gauge-värden via prometheus registry collect() istället för via egen scheduler

--- web/src/main/java/se/inera/intyg/privatlakarportal/service/monitoring/HealthMonitor.java
+++ web/src/main/java/se/inera/intyg/privatlakarportal/service/monitoring/HealthMonitor.java
@@ -18,7 +18,10 @@
  */
 package se.inera.intyg.privatlakarportal.service.monitoring;
 
+import io.prometheus.client.Collector;
 import io.prometheus.client.Gauge;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -28,8 +31,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import se.inera.ifv.privatlakarportal.spi.authorization.impl.HSAWebServiceCalls;
 import se.inera.intyg.privatlakarportal.persistence.model.PrivatlakareId;
@@ -41,6 +42,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.sql.Time;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Exposes health metrics as Prometheus values. To simplify any 3rd party scraping applications, all metrics produced
@@ -52,11 +54,17 @@ import java.util.Collections;
  *
  * Note that NORMAL values uses 0 to indicate OK state and 1 to indicate a problem.
  *
+ * The implementation is somewhat quirky, registering an instace of this class as a Collector, so the
+ * {@link Collector#collect()} method is invoked by the Prometheus registry on-demand. That makes it possible for us
+ * to update the Gauges defined and registered in this collector with new values as part of the normal collect()
+ * lifecycle.
+ *
  * @author eriklupander
  */
-@EnableScheduling
 @Component
-public class HealthMonitor {
+public class HealthMonitor extends Collector {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HealthMonitor.class);
 
     private static final String PREFIX = "health_";
     private static final String NORMAL = "_normal";
@@ -90,8 +98,6 @@ public class HealthMonitor {
             .register();
 
     private static final long MILLIS_PER_SECOND = 1000L;
-
-    private static final long DELAY = 30000L;
     private static final String CURR_TIME_SQL = "SELECT CURRENT_TIME()";
 
     @Value("${app.name}")
@@ -116,11 +122,12 @@ public class HealthMonitor {
     @PostConstruct
     public void init() {
         redisScript = new DefaultRedisScript<>(
-                "return #redis.call('keys','spring:session:"  + appName + ":index:*')", Long.class);
+                "return #redis.call('keys','spring:session:" + appName + ":index:*')", Long.class);
+        this.register();
     }
 
-    @Scheduled(fixedDelay = DELAY)
-    public void healthCheck() {
+    @Override
+    public List<MetricFamilySamples> collect() {
         long secondsSinceStart = (System.currentTimeMillis() - START_TIME) / MILLIS_PER_SECOND;
 
         UPTIME.set(secondsSinceStart);
@@ -128,6 +135,8 @@ public class HealthMonitor {
         USED_HSA_IDS.set(countNbrOfUsedHsaId());
         DB_ACCESSIBLE.set(checkTimeFromDb() ? 0 : 1);
         HSA_WS_ACCESSIBLE.set(pingHsaWs() ? 0 : 1);
+
+        return Collections.emptyList();
     }
 
     private int countSessions() {
@@ -156,13 +165,17 @@ public class HealthMonitor {
     }
 
     private int countNbrOfUsedHsaId() {
-        int nbrOfHsaId = 0;
-        Page<PrivatlakareId> ids = privatlakareIdRepository.findAll(new PageRequest(0, 1, new Sort(Sort.Direction.DESC, "id")));
+        try {
+            int nbrOfHsaId = 0;
+            Page<PrivatlakareId> ids = privatlakareIdRepository.findAll(new PageRequest(0, 1, new Sort(Sort.Direction.DESC, "id")));
 
-        if (!ids.getContent().isEmpty()) {
-            nbrOfHsaId = ids.getContent().get(0).getId();
+            if (!ids.getContent().isEmpty()) {
+                nbrOfHsaId = ids.getContent().get(0).getId();
+            }
+            return nbrOfHsaId;
+        } catch (Exception e) {
+            LOG.error("Error in countNbrOfUsedHsaId: " + e.getClass().getName() + " with message: " + e.getMessage());
+            return -1;
         }
-        return nbrOfHsaId;
     }
-
 }
