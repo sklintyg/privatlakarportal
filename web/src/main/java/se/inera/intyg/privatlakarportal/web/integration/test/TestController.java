0e4161d1ff44bcf03955146f3ecd69933e2c1b29
Peter Larsson <peter.larsson@callistaenterprise.se>
2018-11-27 15:00:47 +0100

    DEVENV: Transaction annotation added for db trans

--- web/src/main/java/se/inera/intyg/privatlakarportal/web/integration/test/TestController.java
+++ web/src/main/java/se/inera/intyg/privatlakarportal/web/integration/test/TestController.java
@@ -23,6 +23,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -97,6 +98,7 @@ public class TestController {
     }
 
     @RequestMapping(value = "/registration/setregistrationdate/{id}", method = RequestMethod.POST)
+    @Transactional
     public boolean setRegistrationDatePrivatlakare(@PathVariable("id") String personId, @RequestBody String date) {
         Privatlakare privatlakare = privatlakareRepository.findByPersonId(personId);
         if (privatlakare == null) {
