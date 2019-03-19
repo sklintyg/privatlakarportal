a2c76b4d7f2ee5f0c9c7500c90fcbf4e9d1a7a83
Magnus Ekstrand <magnus@ekstrand.net>
2019-03-19 12:02:24 +0100

    INTYG-8318: Ersätter Throwabels.propagate som är deprecated

--- integration/wc-integration/src/main/java/se/inera/intyg/privatlakarportal/integration/terms/services/TermsWebServiceCalls.java
+++ integration/wc-integration/src/main/java/se/inera/intyg/privatlakarportal/integration/terms/services/TermsWebServiceCalls.java
@@ -23,8 +23,6 @@ package se.inera.intyg.privatlakarportal.integration.terms.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.google.common.base.Throwables;
 import org.springframework.stereotype.Service;
 import se.riv.infrastructure.directory.privatepractitioner.getprivatepractitionerterms.v1.rivtabp21.GetPrivatePractitionerTermsResponderInterface;
 import se.riv.infrastructure.directory.privatepractitioner.getprivatepractitionertermsresponder.v1.GetPrivatePractitionerTermsResponseType;
@@ -41,14 +39,7 @@ public class TermsWebServiceCalls {
     private static final Logger LOG = LoggerFactory.getLogger(TermsWebServiceCalls.class);
 
     public GetPrivatePractitionerTermsResponseType getPrivatePractitionerTerms() {
-        try {
-            GetPrivatePractitionerTermsType parameters = new GetPrivatePractitionerTermsType();
-            return termsWebServiceClient.getPrivatePractitionerTerms("", parameters);
-        } catch (Exception ex) {
-            LOG.error("Failed to call getPrivatePractitionerTerms");
-            Throwables.propagate(ex);
-            return null;
-        }
+        GetPrivatePractitionerTermsType parameters = new GetPrivatePractitionerTermsType();
+        return termsWebServiceClient.getPrivatePractitionerTerms("", parameters);
     }
-
 }
