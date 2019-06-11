7d4a3d20eaff774b3cc585826e636e9084a4f594
Magnus Ekstrand <magnus@ekstrand.net>
2019-06-11 16:59:03 +0200

    DEVENV: Monterad ned XML config till förmån för Java config

--- integration/hsa-integration/src/main/java/se/inera/intyg/privatlakarportal/hsa/config/HsaConfiguration.java
+++ integration/hsa-integration/src/main/java/se/inera/intyg/privatlakarportal/hsa/config/HsaConfiguration.java
@@ -18,16 +18,36 @@
  */
 package se.inera.intyg.privatlakarportal.hsa.config;
 
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
+import se.inera.ifv.hsaws.v3.HsaWsResponderInterface;
+
 @Configuration
-@ComponentScan({ "se.inera.intyg.privatlakarportal.hsa.services", "se.inera.intyg.privatlakarportal.hsa.monitoring",
-        "se.inera.intyg.privatlakarportal.common.config" })
+@ComponentScan({
+        "se.inera.ifv.privatlakarportal.spi.authorization.impl",
+        "se.inera.intyg.privatlakarportal.hsa.services",
+        "se.inera.intyg.privatlakarportal.hsa.monitoring",
+        "se.inera.intyg.privatlakarportal.common.config"
+})
 @Import(HsaStubConfiguration.class)
 @ImportResource("classpath:hsa-services-config.xml")
 public class HsaConfiguration {
+
+    @Value("${hsa.ws.services.url}")
+    private String hsaWsUrl;
+
+    @Bean
+    public HsaWsResponderInterface hsaWebServiceClient() {
+        JaxWsProxyFactoryBean proxyFactoryBean = new JaxWsProxyFactoryBean();
+        proxyFactoryBean.setAddress(hsaWsUrl);
+        proxyFactoryBean.setServiceClass(HsaWsResponderInterface.class);
+        return (HsaWsResponderInterface) proxyFactoryBean.create();
+    }
 
 }
