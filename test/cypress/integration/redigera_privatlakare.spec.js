61b0ea495942e6d08f4d7cbd073de4739233eb16
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2019-06-20 09:47:22 +0200

    INTYGFV-11979: Uppdaterat testet, kanske blir bättre men borde bli enklare att debuga (#17)

--- test/cypress/integration/redigera_privatlakare.spec.js
+++ test/cypress/integration/redigera_privatlakare.spec.js
@@ -6,12 +6,15 @@ describe('Redigera Privatläkare', function() {
 
         //Sapa privatläkare via REST
         cy.skapaPrivatlakare().its('status').should('eq', 200);
+        cy.log('User created')
 
         //Logga in som privatläkare
         cy.login('0');
+        cy.log('User logged in')
 
         //Gå till min sida
         cy.get('#minsida').should('be.visible');
+        cy.log('Page loaded')
 
         //Fyll i registrering
         cy.get('#befattning').select('202010');
@@ -25,9 +28,13 @@ describe('Redigera Privatläkare', function() {
         cy.get('#adress').clear().type('Gatuadressen');
         cy.get('#postnummer').clear().type('13100');
         cy.get('#saveBtn').click();
+    });
 
+    it('verifiera', function() {
+        cy.wait(200)
         //Logga in som privatläkare
         cy.login('0');
+        cy.log('User logged in')
 
         //Gå till min sida
         cy.get('#minsida').should('be.visible');
