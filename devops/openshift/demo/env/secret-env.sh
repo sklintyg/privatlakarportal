12a6358b70328bc8e95842afcce14fb189590824
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2019-10-08 11:01:28 +0200

    INTYGFV-11646: Lagt till filer som saknas (#45)

---
+++ devops/openshift/demo/env/secret-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export CATALINA_OPTS_APPEND="\
+-Dspring.profiles.active=$SPRING_PROFILES_ACTIVE \
+-Dconfig.folder=/opt/$APP_NAME/config \
+-Dconfig.file=/opt/$APP_NAME/config/privatlakarportal.properties \
+-Dmailresource.file=/opt/$APP_NAME/config/mailresource.properties \
+-Dlogback.file=$LOGBACK_FILE \
+-Dcertificate.folder=/opt/$APP_NAME/certifikat \
+-Dcredentials.file=/opt/$APP_NAME/env/secret-env.properties \
+-Dresources.folder=classpath: \
+-Dfile.encoding=UTF-8 \
+-DbaseUrl=http://${APP_NAME}:8080"
