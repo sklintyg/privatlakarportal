0a3b3c5701980f924078e36e15f4217799ecc308
cigerhed <christoffer.sigerhed@soprasteria.com>
2024-11-08 16:34:22 +0100

    K1J-23: Update constant and dependencies.

--- logging/src/main/java/se/inera/intyg/privatlakarportal/logging/MdcLogConstants.java
+++ logging/src/main/java/se/inera/intyg/privatlakarportal/logging/MdcLogConstants.java
@@ -39,7 +39,7 @@ public class MdcLogConstants {
     public static final String EVENT_OUTCOME_FAILURE = "failure";
     public static final String EVENT_OUTCOME_SUCCESS = "success";
     public static final String EVENT_USER_HSA_ID = "event.user.hsa.id";
-    public static final String EVENT_USER_ID = "user.id";
+    public static final String EVENT_USER_ID = "event.user.id";
     public static final String SESSION_ID_KEY = "session.id";
     public static final String SPAN_ID_KEY = "span.id";
     public static final String TRACE_ID_KEY = "trace.id";
